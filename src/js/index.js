6c15bafe972993317f1237023b922bd665db4e8d
maslovmichail20 <maslov.mikhail2000@gmail.com>
2018-11-25 15:54:19 +0200

    Edit index

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,24 @@
-console.log('hello');
+import * as diagram from './diagram';
+import fetchPackageInfo from './fetchPackageInfo';
+import changeDepTree from './changeDepTree';
+
+const getSimpleInfo = (name) => {
+  return fetch('https://api.npms.io/v2/package/' + name)
+    .then(response => response.json())
+};
+
+getSimpleInfo('react').then(data => {
+  console.log(data);
+});
+
+fetchPackageInfo('react').then(depTree => {
+  console.log(depTree);
+  const tree = changeDepTree(depTree, 'npm', 'downloads');
+  console.log(tree);
+  diagram.render(tree);
+});
+
+
+
+
+
