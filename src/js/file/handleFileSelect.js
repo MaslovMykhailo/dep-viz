69f9d9c3f89dd8006d1f1784229d8090682e6440
maslovmichail20 <maslov.mikhail2000@gmail.com>
2019-01-02 10:54:29 +0200

    Add change file handling

--- src/js/file/handleFileSelect.js
+++ src/js/file/handleFileSelect.js
@@ -1,3 +1,6 @@
+import diagram from '../diagram/diagram';
+import { rebuild } from '../diagram/actions';
+
 export default (mainActionFlow, anotherActionFlow) => (event) => {
   const file = event.target.files[0];
   if (file.type !== 'application/json') {
@@ -10,6 +13,8 @@ export default (mainActionFlow, anotherActionFlow) => (event) => {
   reader.onload = function(e) {
     const data = JSON.parse(e.target.result);
     if (data.dependencies) {
+      if (diagram.dataTree) rebuild();
+
       mainActionFlow({
         name: data.name,
         dependencies: Object.keys(data.dependencies),
